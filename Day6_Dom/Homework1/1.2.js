72406e2288756b945a440253c8d7ed018f82b622
Gravityfake <103029194+Gravityfake@users.noreply.github.com>
2022-07-08 13:05:25 +0700

    Create 1.2.js

---
+++ Day6_Dom/Homework1/1.2.js
@@ -0,0 +1,16 @@
+const content = document.getElementById("content")
+const inp = document.getElementById("inp")
+
+const someone = ()=>{
+
+    const num  = inp.value
+    const pad = /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g
+    let ans = num.toString().replace(",","").replace( pad, ",");
+
+
+    content.innerText = ans
+    inp.value = ans
+}
+
+inp.addEventListener('keyup',someone)
+
