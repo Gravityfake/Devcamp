debd3ecf1d5b8ea5436f783d3866480647860c50
Gravityfake <103029194+Gravityfake@users.noreply.github.com>
2022-10-09 03:46:14 +0700

    Update App.js

--- Day23 React Life Cycle/homework3/src/App.js
+++ Day23 React Life Cycle/homework3/src/App.js
@@ -1,105 +1,25 @@
-import "antd/dist/antd.css";
-import { Routes, Route, Outlet, Link } from "react-router-dom";
-import Employee from "./Employee";
-import EmployeeDetail from "./EmployeeDetail";
-import Department from "./Department";
-import EmployeeList from "./data";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return <Routing />;
-}
-
-function Routing() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="employee" element={<Employee />} />
-        <Route
-          //path="employee/:id/:firstname/:lastname/:position/:department"
-          path="employee-detail"
-          element={<EmployeeDetail />}
-        />
-        <Route path="department" element={<Department />} />
-        <Route path="employee/:department" element={<Employee />} />
-        <Route path="*" element={<NotMatch />} />
-      </Route>
-    </Routes>
-  );
-}
-
-function Layout() {
-  return (
-    <ContainerComp>
-      <HeaderComp></HeaderComp>
-      <MenuComp></MenuComp>
-      <ContentComp></ContentComp>
-      <FooterComp></FooterComp>
-    </ContainerComp>
-  );
-}
-
-function ContainerComp(props) {
-  return props.children;
-}
-
-function HeaderComp() {
-  return (
-    <>
-      <h1>Header</h1>
-      <hr />
-    </>
-  );
-}
-
-function MenuComp() {
-  return (
-    <>
-      <h1>Menu</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/employee">Employee List</Link>
-          </li>
-          <li>
-            <Link to="/department">Department List</Link>
-          </li>
-        </ul>
-      </nav>
-      <hr />
-    </>
-  );
-}
-
-function ContentComp() {
-  return (
-    <>
-      <Outlet />
-      <hr />
-    </>
-  );
-}
-
-function FooterComp() {
-  return (
-    <div style={{ marginLeft: "20px" }}>
-      <h1>Contact</h1>
-      <p>Tell:0844567789</p>
-      <p>E-mail:shop@gmail.com</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function NotMatch() {
-  return <h2>Not Match</h2>;
 }
 
 export default App;
