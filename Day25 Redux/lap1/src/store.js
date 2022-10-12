f7c32dd932974d415baa3bb7a63f95751f61a3fb
Gravityfake <103029194+Gravityfake@users.noreply.github.com>
2022-10-13 00:09:06 +0700

    Update store.js

--- Day25 Redux/lap1/src/store.js
+++ Day25 Redux/lap1/src/store.js
@@ -1,13 +1,12 @@
-import { createStore} from 'redux'
-import { configureStore } from '@reduxjs/toolkit'
-import calculateReducer from './calculateReducer'
+import { configureStore } from "@reduxjs/toolkit";
+import calculateReducer from "./calculateReducer";
+import profileReducer from "./profileReducer";
 
 const store = configureStore({
-    reducer: {
-        counter: calculateReducer,
-        profile: calculateReducer
-    }
-})
+  reducer: {
+    counter: calculateReducer,
+    profile: profileReducer,
+  },
+});
 
-export default store
-
+export default store;
